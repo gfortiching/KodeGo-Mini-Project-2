b82b9f4f0e5b2ceb62f320faca8556c07677e973
Gab <gabrielfabilena>
2022-11-29 14:36:21 +0800

    Added Images Folder

--- Wizard Quiz/scripts/questions.js
+++ Wizard Quiz/scripts/questions.js
@@ -6,12 +6,19 @@ function getBotResponse(input) {
     }
 
     else if (input == "Not yet.") {
-        return "I understand. You can go back to our lessons and have a review.";
+        return "I understand. You can go back to our lessons and take a review.";
     }
 
     else if (input == "50") {
         let answer1 = 5 * (5 + 5);
-        return "Great job! The answer is " + answer1 + ".";
+        return "Great job! The answer is " + answer1 + ".<br><br>What is 20 + 25?" ;
+        score()
+    } 
+
+    else if (input == "45") {
+        let answer2 = 20 + 25;
+        return "Nice work! The answer is " + answer2 + ".";
+        score()
     } 
 
     else {
